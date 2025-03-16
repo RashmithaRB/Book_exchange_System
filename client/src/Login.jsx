a89fbb4f73ec256cc6320c4f90da7a10b2c914f9
RashmithaRB <rbangerarashmitha@gmail.com>
2025-03-16 15:15:08 +0530

    Login and register frontend and backend updated

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,18 +1,22 @@
+import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
-function Signup(){
-    const[name,setName]=useState()
+function Login(){
     const[email,setEmail]=useState()
     const[password,setPassword]=useState()
     const navigate=useNavigate()
 
     const handleSubmit=(e)=>{
         e.preventDefault()
-        axios.post('http://localhost:3001/login',{name,email,password})
-        .then(result=>{console.log(result)
-            navigate('/home')
+        axios.post('http://localhost:3001/login',{email,password})
+        .then(result=>{
+            console.log(result)
+            if(result.data=="Success"){
+                navigate('/home')
+            }
+            
         })
         .catch(err=>console.log(err))
 
@@ -20,8 +24,9 @@ function Signup(){
     return(
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg-white p-3 rounded w-25">
-                <h2>Register</h2>
+                <h2>Login</h2>
                 <form onSubmit={handleSubmit}>
+                    
                     <div className="mb-3">
                         <label htmlFor="email">
                             <strong>Email</strong>
@@ -47,12 +52,12 @@ function Signup(){
 
                     </div>
                     <button type="submit" className="btn btn-success w-100 rounded-0">
-                        Register
+                        Login
                     </button>
                     </form>
-                    <p>Already Have An Account</p>
-                    <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-                        Login
+                    <p>Do not have an account</p>
+                    <Link to="/register" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                        Sign Up
                     </Link>
                 
             </div>
@@ -60,4 +65,4 @@ function Signup(){
         </div>
     );
 }
-export default Signup;
+export default Login;
